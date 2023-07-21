389874d6159452559c4acb18dd67d6bec61fd203
邱宏康 <3290173703@qq.com>
2023-07-21 18:29:13 +0800

    代码初始化

---
+++ run.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class='main_box'>
+        <audio src="../assets/img/music.mp3" controls></audio>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        created() {},
+        mounted() {},
+        methods: {},
+    };
+
+</script>
+
+<style scoped lang='less'>
+* {
+    box-sizing: border-box;
+}
+    .main_box {
+        padding: 20px;
+        width: 100%;
+        height: 100%;
+        .audio {
+            width: 100%;
+        }
+    }
+
+</style>
